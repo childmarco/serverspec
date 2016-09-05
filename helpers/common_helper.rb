f818c3babc971f42f0d3b881733eb716d7a8420a
chikashini <chikashi.niiyama@accenture.com>
2016-09-05 10:12:36 +0900

    delete unnecesarry code

--- helpers/common_helper.rb
+++ helpers/common_helper.rb
@@ -4,23 +4,9 @@ require 'json'
 
 module CommonHelper
 
-  @@testing_hostname_condition = YAML.load_file(File.join(__dir__, '../property/properties.yml'))
 
   def get_csv_config(hostname, layer, target)
 
-    spec_file_path = caller[0][/^([^:]+):\d+:in `[^']*'$/, 1]
-    # spec_file      = File.basename(__FILE__).match(/(^.+?)_spec.rb/)
-    spec_file      = File.basename(spec_file_path).match(/(^.+?)_spec.rb/)[1]
-    target_layer   = spec_file_path.split("/")[-2]
-
-    p spec_file[1]
-    p target_layer[-2]
-
-
-    config_file = @@testing_hostname_condition[hostname][layer][target]
-
-
-    # csv = CSV.read(File.expand_path('../../property/properties.yml', __FILE__), header: true, skip_blanks: true)
 
   end
 
